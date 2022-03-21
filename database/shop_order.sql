fc5e579b16561bd37513fc2fa000012af7136ba5
Ellen <98078487+yyellen@users.noreply.github.com>
2022-03-21 10:38:56 +0800

    Update shop_order.sql

--- database/shop_order.sql
+++ database/shop_order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2022-03-18 18:03:00
+-- 產生時間： 2022-03-21 10:35:36
 -- 伺服器版本： 10.4.22-MariaDB
 -- PHP 版本： 8.1.2
 
@@ -31,11 +31,21 @@ CREATE TABLE `shop_order` (
   `id` int(5) NOT NULL,
   `user_id` int(3) NOT NULL,
   `product_id` int(3) NOT NULL,
+  `product_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_520_ci NOT NULL,
+  `price` int(5) NOT NULL,
   `amount` int(3) NOT NULL,
   `delivery` varchar(50) NOT NULL,
   `order_time` datetime NOT NULL,
   `valid` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- 傾印資料表的資料 `shop_order`
+--
+
+INSERT INTO `shop_order` (`id`, `user_id`, `product_id`, `product_name`, `price`, `amount`, `delivery`, `order_time`, `valid`) VALUES
+(1, 1, 1, '頂級柔軟都市氛圍圖案床包枕套組', 2880, 2, '', '2022-03-21 10:29:43', 1),
+(2, 1, 4, '新鮮空運極品挪威生鮭魚片5片組', 1265, 3, '', '2022-03-21 10:29:43', 1);
 
 --
 -- 已傾印資料表的索引
@@ -55,7 +65,7 @@ ALTER TABLE `shop_order`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `shop_order`
 --
 ALTER TABLE `shop_order`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
